9c06043922edd37e1fa05ff33ebc582d9e35cfe0
sanchflight <diego.sp10@outlook.es>
2022-01-24 15:24:40 +0100

    commit 5 -function is working & ready

--- src.py
+++ src.py
@@ -1,15 +1,19 @@
 def row_nan_out(x,y):
     #this function eliminate all rows of a dataframe for which a given column's value is null
     #argument "x" is supposed to capture the df's name and "y" the name of the refference column
+    #if the function does not find null values in the specified column it returns nothing
     x_nans = x[y].isna()
     
-    if "True" in x_nans:
-        print("Se encontaron valores nulos a eliminar en los datos.")
-        for i in range(0, len(x_nans)):
-            if x_nans[i] == True:
-                x.drop(i, axis=0, inplace=True)
-        return x
+    for i in x_nans:
+        if i == True:
+            print("Se encontaron valores nulos a eliminar en los datos.")
+            for j in range(0, len(x_nans)):
+                if x_nans[j] == True:
+                    x.drop(j, axis=0, inplace=True)
+            return x
 
-    else:
-        print("No se encontraron valores nulos en los datos proporcionados.")
-        return x
+        else:
+            pass
+    return x
+    #piensa en incluir algo de programación defensiva y gestion de errores para que-
+    #-la función te quede niquelada
